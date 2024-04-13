160789fd059b0061631edc3007984e211972ba24
Afrem <afremer636@gmail.com>
2024-04-13 15:43:52 +0300

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const urun = document.getElementsByClassName('urun');
-const btn6 = document.getElementsByClassName("btn6");
-const btnCart = document.querySelector(".btn-cart");
-const cartList = document.querySelector(".shopping-cart-list");
-
+// Ürün bilgilerini tutan sınıf
 class ShoppingItem {
     constructor(image, title, price) {
         this.image = image;
@@ -11,71 +7,95 @@ class ShoppingItem {
     }
 }
 
+// UI işlemlerini yöneten sınıf
 class UI {
-    addToCart(shopping) {
+    constructor() {
+        this.cartList = document.querySelector(".shopping-cart-list");
+        this.itemCount = document.getElementById("item-count");
+    }
+
+    // Sepete yeni bir ürün ekler
+    addToCart(item) {
         const listItem = document.createElement("div");
-        listItem.classList = "list-item";
-
-        listItem.innerHTML =
-        `<div class="list-item">
+        listItem.classList.add("list-item");
+        listItem.innerHTML = `
             <div class="row align-items-center text-white-50">
-              <div class="col-md-3">
-                <img src="${shopping.image}" alt="product" class="img-fluid">
-              </div>
-              <div class="col-md-5">
-                <div class="title">${shopping.title}</div>
-              </div>
-              <div class="col-md-2">
-                <div class="price">${shopping.price}</div>
-              </div>
-              <div class="col-md-2">
-                <button class="btn btn-delete fs-6"><i class="fas fa-trash-alt text-danger btn-delete"></i></button>
-              </div>
-            </div>
-        </div>`
-
-        cartList.appendChild(listItem);
-        this.removeCart(listItem);
-        this.cartCount();
+                <div class="col-md-3">
+                    <img src="${item.image}" alt="product" class="img-fluid">
+                </div>
+                <div class="col-md-5">
+                    <div class="title">${item.title}</div>
+                </div>
+                <div class="col-md-2">
+                    <div class="price">${item.price}</div>
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-delete fs-6">
+                        <i class="fas fa-trash-alt text-danger btn-delete"></i>
+                    </button>
+                </div>
+            </div>`;
+        this.cartList.appendChild(listItem);
+        this.updateCartCount();
     }
 
-    removeCart(item) {
-        let btnRemove = item.querySelector(".btn-delete");
-        btnRemove.addEventListener("click", function() {
-            item.remove();
-            this.cartCount();
-        }.bind(this))
+    // Sepetten bir ürünü kaldırır
+    removeCartItem(event) {
+        event.target.closest('.list-item').remove();
+        this.updateCartCount();
     }
 
-    cartCount() {
-        let cartListItem = cartList.getElementsByClassName("list-item");
-        let itemCount = document.getElementById("item-count");
-        itemCount.innerHTML = cartListItem.length;
+    // Sepetteki ürün sayısını günceller
+    updateCartCount() {
+        const cartItems = this.cartList.getElementsByClassName('list-item');
+        this.itemCount.textContent = cartItems.length;
     }
 }
 
-for (let i = 0; i < urun.length; i++) {
-    btn6[i].addEventListener("click", function(e) {
-        let title = urun[i].getElementsByClassName("title")[0].textContent;
-        let price = urun[i].getElementsByClassName("price")[0].textContent;
-        let image = urun[i].getElementsByClassName("product")[0].src;
-        btn6[i].classList.add("disabled");
-        btn6[i].textContent = "Eklendi";
-        let shopping = new ShoppingItem(image, title, price);
-        let ui = new UI();
+// UI sınıfının bir örneğini oluştur
+const ui = new UI();
 
-        ui.addToCart(shopping);
-
-        e.preventDefault();
-    })
+// Sepete ekleme butonlarına tıklandığında çalışacak fonksiyon
+function addToCartEvent() {
+    const addToCartButtons = document.getElementsByClassName("btn6");
+    for (let button of addToCartButtons) {
+        button.addEventListener("click", function(event) {
+            const product = event.target.closest('.urun');
+            const title = product.getElementsByClassName("title")[0].textContent;
+            const price = product.getElementsByClassName("price")[0].textContent;
+            const image = product.getElementsByClassName("product")[0].src;
+            const shoppingItem = new ShoppingItem(image, title, price);
+            ui.addToCart(shoppingItem);
+            button.classList.add("disabled");
+            button.textContent = "Eklendi";
+            event.preventDefault();
+        });
+    }
 }
 
-
+// Sepeti göster/gizle butonu için olay dinleyicisi
 function cartToggle() {
+    const btnCart = document.querySelector(".btn-cart");
     btnCart.addEventListener("click", function() {
-        cartList.classList.toggle("d-none");
-    })
+        ui.cartList.classList.toggle("d-none");
+    });
 }
 
+// Sepetten ürün kaldırma butonları için olay dinleyicisi
+function removeCartItemEvent() {
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('btn-delete')) {
+            ui.removeCartItem(event);
+        }
+    });
+}
 
-cartToggle();
+// Uygulama başlatıldığında çalışacak fonksiyonlar
+function initializeApp() {
+    addToCartEvent();
+    cartToggle();
+    removeCartItemEvent();
+}
+
+// Uygulamayı başlat
+initializeApp();
